efc201c370e7a3b1d9f665af518cb4b80c33c87c
H4RIDADELX <guisilva789n@gmail.com>
2023-10-17 21:51:02 -0300

    JS2

---
+++ JS/Aula02-Variaveis/main.js
@@ -0,0 +1,44 @@
+var nomeDoPrimeiroAluno = "Guilherme"
+
+console.log(nomeDoPrimeiroAluno);
+
+let numeroDeAproavados = 5
+
+console.log(numeroDeAproavados);
+
+console.clear()
+
+const nomeSiteWeb = "facebook"
+
+
+console.log(nomeSiteWeb)
+
+
+
+// Exibir a soma de dois numeros
+// console.log(2 + 4)
+
+var numero1 = 6;
+
+var numero2 = 4;
+
+var soma = numero1 + numero2;
+
+console.log(soma)
+
+
+
+// string
+var texto1 = "No mundo atual, o acompanhamento das preferências de consumo prepara-nos para enfrentar situações atípicas "
+
+var texto2 = "decorrentes do investimento em reciclagem técnica."
+
+var concatenar = texto1 + "\n" + texto2;
+console.log(concatenar)
+
+console.clear()
+
+var osDeVerdadeAgenteSabeQuemE = false;
+
+console.log("5" - 1 )
+console.log(1 +"5")
