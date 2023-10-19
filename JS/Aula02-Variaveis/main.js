e11af31d780596ae7d5401c0a66f42a9bb7af64f
H4RIDADELX <guisilva789n@gmail.com>
2023-10-18 21:39:55 -0300

    JS2cont

--- JS/Aula02-Variaveis/main.js
+++ JS/Aula02-Variaveis/main.js
@@ -42,3 +42,35 @@ var osDeVerdadeAgenteSabeQuemE = false;
 
 console.log("5" - 1 )
 console.log(1 +"5")
+
+console.clear()
+
+// incremento
+var incremento = 2;
+
+// incremento = incremento + 1
+incremento+=7
+
+console.log(incremento)
+
+var imparPar = 4%2;
+
+console.log("Se o resultado for 1 então o numero é impar se o resultado for 0 então é par, esse é o resultado = " + imparPar )
+
+console.clear()
+
+var comparacao = 2 < 3
+
+console.log ("2 é menor que 3 ?  " + comparacao)
+
+var doisSinaisDeIguais = 3 != 5
+
+// texto, numero
+
+// 3 == 3 // true
+// numerico = numerico // true
+
+
+console.log(doisSinaisDeIguais)
+
+var addNunber 
