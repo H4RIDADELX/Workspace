65fe299fa39dfe603476f0ea4bc58b96eaaf3b02
H4RIDADELX <guisilva789n@gmail.com>
2023-10-19 21:45:07 -0300

    JS03

---
+++ JS/Aula03-Strings/main.js
@@ -0,0 +1,56 @@
+var texto = "Texto bem aleatorio dos atrasados da volta da pausa"
+
+var texto = "Texto bem aleatorio dos atrasados da volta da pausa"
+
+// console.log (texto1 + " " "A vida é um sapinho dando pulinho na lagoa" + " " + texto2)
+
+// templete string
+// console.log(`${texto1} texto aleatorio ${texto2}`)
+
+var numero1 = 2;
+var numero2 = 3;
+
+console.log(`${numero1 + numero2 }`)
+console.log(numero1 + numero2)
+
+console.clear()
+
+const String01 = "Instituto da oportunidade Social"
+
+console.log(String01)
+console.log("Posicao 10 " + String01.charAt(10))
+
+console.log("Posicao 11 " + String01.charAt(11))
+
+console.clear()
+
+const String02 = "Instituto da oportunidade Social"
+
+console.log("Quantidade de caracteres: " + String02.length)
+
+console.clear()
+
+const String03 = "Instituto da oportunidade Social"
+
+console.log("Trazer em minusculo :" + String03.toLowerCase ())
+
+var sub = "Mozilla"
+
+console.log(sub.substring(2 ,7));
+console.log(sub.substring(5));
+
+console.clear()
+
+
+var trocaTexto = "Bem vindo ao jogo, Roblox";
+
+let textoTrocado = trocaTexto.replace("Roblox", "KlebinhoJogador2028")
+
+console.log("Texto original: " + trocaTexto);
+console.log("Texto alterado: " + textoTrocado);
+
+console.clear()
+
+var retirandoEspacos = "         A vida é uma uva    "
+
+console.log(retirandoEspacos.trim())
