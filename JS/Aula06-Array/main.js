a37cbda5f0d958ac4f1c147bd56f9fde556454eb
H4RIDADELX <guisilva789n@gmail.com>
2023-10-26 21:51:47 -0300

    JS06

---
+++ JS/Aula06-Array/main.js
@@ -0,0 +1,71 @@
+var numero = [54,111,58,853,17,75,30]
+//array unidimensional
+//hora do lanche
+
+//console.log(numeros)
+console.log("Antes" + numero)
+numero[3] = 'Hora do lanche'
+console.log("Depois" + numero)
+
+
+var matrix = [
+    ["Icarly", 2011, 10],
+    ["os Fet de EverlyPlace", 2009,10],
+    ["Doug", 1999,8.7]
+]
+//console.log(matrix)
+console.log("Antes" + matrix)
+matrix[0][2] = 1800
+console.log("Depois" + matrix)
+console.log(matrix[2][1])
+
+
+console.clear()
+
+
+var numArray = [2,3,6,7,45,86]
+
+console.log(numArray)
+
+
+
+
+
+
+// //Metodo para add no inicio
+// let insereInicio = numArray.unshift("pipoca")
+// console.log(numArray)
+// console.log(insereInicio)
+
+
+
+
+// Remover no inicio do array
+// let retirarInicio = numArray.shift()
+// console.log(numArray)
+// console.log(retirarInicio)
+
+
+
+
+// Metodo ADD nova informação no final do Array
+// let novaInformacao = numArray.push("Kiwi")
+// console.log(numArray)
+// console.log(novaInformacao)
+
+//Metodo para retirar o ultimo elemento
+// let retiraUltimoElemento = numArray.pop() //86
+// console.log(retiraUltimoElemento)
+// console.log(numArray)
+
+
+// console.log(numArray.length)
+
+//metodo Join
+// let testeJoin = numArray.join(".")
+// console.log(testeJoin)
+
+
+// metodo tostring
+// let receba = numArray.toString()
+// console.log(receba)
