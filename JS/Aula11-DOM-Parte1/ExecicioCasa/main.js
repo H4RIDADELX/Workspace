b32d1d7cbff3908c63b3c3b778c488485275e68c
guilhermeS1lva <guisilva789n@gmail.com>
2023-11-08 21:19:17 -0300

    JS11

---
+++ JS/Aula11-DOM-Parte1/ExecicioCasa/main.js
@@ -0,0 +1,7 @@
+document.title = "Cadastro empresa Macron";
+
+let h1 = document.createElement('h1');
+
+document.body.appendChild(h1);
+
+h1.innerHTML = "Cadastro do usuario"
---
+++ JS/Aula11-DOM-Parte1/main.js
@@ -0,0 +1,10 @@
+// cricao da variavel e criacao do botao
+let botao = document.createElement("BUTTON");
+// adicionar o filho(botao) na body
+document.body.appendChild(botao)
+
+
+botao.innerHTML = 'Acessa ai';
+// borde-bottom
+document.body.style.backgroundColor = 'pink'
+botao.style.border = 'solid 2px blue';
