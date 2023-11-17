0ae679889c2fc6af7b8d34e6dd59155f4aa34397
guilhermeS1lva <guisilva789n@gmail.com>
2023-11-17 20:27:22 -0300

    A

--- React/primeiro-projeto-react/src/App.js
+++ React/primeiro-projeto-react/src/App.js
@@ -1,27 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import BoasVindas from './components/BoasVindas';
 import Exemplo from './components/Exemplo';
 
 function App() {
   return (
-    <Exemplo/>
-    // <div className="App">
-    //   {/* <h1>Estou sofrendo para aprender React</h1> */}
-    //   {/* <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header> */}
-    // </div>
+    <div className="App">
+      <BoasVindas/>
+      <Exemplo/>
+    
+  </div>
   );
 }
 
--- React/primeiro-projeto-react/src/components/Exemplo.jsx
+++ React/primeiro-projeto-react/src/components/Exemplo.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+let estilo = {listStyle: 'none', color: 'blue', fontSize: '67px'};
 
-const Exemplo = ()=> <h1>Estou sofrendo para aprender React</h1>;
+const Exemplo = ()=> {
+    return (
+        <>
+        <h1>Sexta-feira depressão</h1>
+        <p>Metade  vai reprovar</p>
+        <ul style={estilo}>
+            <li>Maria Eduarda</li>
+            <li>Anna Clara</li>
+            <li>Leandro Jun</li>
+            <li>O Restante da sala</li>
+        </ul>
+        
+        </>
+    )
+};
 
 export default Exemplo;
