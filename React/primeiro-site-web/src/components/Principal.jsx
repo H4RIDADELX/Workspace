3377d9d9fc0fee2b90b7aa8dd0ea8fe4bc8180db
guilhermeS1lva <guisilva789n@gmail.com>
2023-11-20 11:29:37 -0300

    add img React

--- React/primeiro-site-web/src/components/Principal.jsx
+++ React/primeiro-site-web/src/components/Principal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 let estilo = {fontSize: '2rem', color: 'purple', backgroundColor:'orange'}
+let img = {witdh:'200px', height:'200px'}
 
 const Principal = ()=> {
     return (
@@ -8,7 +9,7 @@ const Principal = ()=> {
             Estou participando do curso de IOS com o objetivo de adquirir conhecimentos fundamentais na área de desenvolvimento front-end.Meu intuito é aprender a coloborar eficientemente em projetos de equipe, aprimorar minhas habilidades de comunicação e, a longo prazo, evoluir como desenvolvedor
         </p>
         <ol>
-            <li>+ Velozes + Furiosos</li>
+            <li><img style={img}  src="https://www.planocritico.com/wp-content/uploads/2015/11/154-Velozes-Furiosos.jpg" alt="" /></li>
             <li>Sempre ao seu lado</li>
             <li>Carros</li>
             <li>Pantera Negra</li>
